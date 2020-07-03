ab9ccc00db1b182d52732dc233b49fd5b0020107
imamrmdn <imamramadhan420@gmail.com>
2020-07-03 17:58:37 +0700

    update ui

--- BLoC_5/lib/ui/widgets/user_card.dart
+++ BLoC_5/lib/ui/widgets/user_card.dart
@@ -1,4 +1,5 @@
 import 'package:BLoC_5/models/user_model.dart';
+import 'package:BLoC_5/ui/pages/second_page.dart';
 import 'package:flutter/material.dart';
 
 class UserCard extends StatelessWidget {
@@ -9,37 +10,43 @@ class UserCard extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        width: double.infinity,
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          children: <Widget>[
-            Container(
-              width: 75.0,
-              height: 75.0,
-              padding: EdgeInsets.only(right: 10.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  image: NetworkImage(user.avatar),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SecondPage()));
+      },
+      child: Card(
+        child: Container(
+          width: double.infinity,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 75.0,
+                height: 75.0,
+                padding: EdgeInsets.only(right: 10.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(user.avatar),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 50),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('id: ${user.id}'),
-                Text('Nama: ${user.name}'),
-                Text('Email: ${user.email}'),
-              ],
-            ),
-          ],
+              SizedBox(width: 50),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('id: ${user.id}'),
+                  Text('Nama: ${user.name}'),
+                  Text('Email: ${user.email}'),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
